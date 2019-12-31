a4829d40545ca4456e77661e54f404f3f937580c
iotcat <i@iotcat.me>
2019-12-31 11:41:33 +0000

    Committed by cron-git auto

---
+++ index.js
@@ -0,0 +1,18 @@
+const cron = require('node-schedule');
+const git = require('simple-git')();
+
+
+
+/*
+	cron.scheduleJob('1-50 * * * * *', function(){
+		console.log('scheduleCronstyle:' + new Date());
+	});
+*/
+
+
+git.pull('origin', 'master', {'--no-rebase': null});
+
+
+git.add('./*')
+   .commit("Committed by cron-git auto")
+   .push(['-u', 'origin', 'master'], () => console.log('git push done!!'));
