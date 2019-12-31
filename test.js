91f66349ef281c92f69be498cf46976ef3be5ad4
iotcat <i@iotcat.me>
2019-12-31 13:24:10 +0000

    Committed by cron-git from Yimian-PC

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 const crongit = require('./index.js')();
 
-crongit.schedule("10 * * * * *", ()=>console.log('test ok!!!'));
+crongit.schedule("10 * * * * *", ()=>console.log('test ok!!!\nuse Ctrl+C to exit!!'));
